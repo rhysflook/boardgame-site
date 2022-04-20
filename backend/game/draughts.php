b15448c88f8c3e1d53680a8d9fe2fcc78117937b
Rhys Flook <rhys.flook@googlemail.com>
2022-04-20 13:47:37 +0900

    got the right scope

--- backend/game/draughts.php
+++ backend/game/draughts.php
@@ -48,7 +48,7 @@
       const registration = await navigator.serviceWorker.register(
         '../../service-worker.js',
         {
-          scope: '../../draughts',
+          scope: '../../',
         }
       );
       if (registration.installing) {
